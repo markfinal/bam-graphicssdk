3f6a988bcb09c0991cb45878b59a6a152bb029cc
Mark Final <markfinal@hotmail.com>
2015-09-18 21:39:42 +0100

    [tests] Adding graphics tests on Windows

---
+++ tests/bamtests.py
@@ -0,0 +1,8 @@
+from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+
+def ConfigureRepository():
+    configs = {}
+    configs["Direct3DTriangle"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]})
+    configs["OpenGLTriangle"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
+    configs["RenderTextureAndProcessor"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
+    return configs
