ac7e8e2536e1a4dd631e2ffe835c7d9ebe53954d
Mark Final <markfinal@hotmail.com>
2015-11-13 10:06:38 +0000

    [packages] Adding glew-1.13.0

---
+++ packages/glew-1.13.0/bam/Scripts/glew.cs
@@ -0,0 +1,39 @@
+#region License
+// <copyright>
+//  Mark Final
+// </copyright>
+// <author>Mark Final</author>
+#endregion // License
+using Bam.Core;
+namespace glew
+{
+    [Bam.Core.ModuleGroup("Thirdparty")]
+    sealed class GLEWStatic :
+        C.StaticLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCSourceContainer();
+            source.AddFile("$(packagedir)/src/glew.c");
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/include"));
+                        compiler.PreprocessorDefines.Add("GLEW_STATIC");
+                        compiler.PreprocessorDefines.Add("GLEW_NO_GLU");
+                    }
+                });
+
+            if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+            {
+                this.CompileAgainst<WindowsSDK.WindowsSDK>(source);
+            }
+        }
+    }
+}
