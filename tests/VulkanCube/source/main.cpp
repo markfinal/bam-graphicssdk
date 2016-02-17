c6e6e80dc81b9dad4dbc261816bdcdc1793415ef
Mark Final <markfinal@hotmail.com>
2016-02-17 17:35:23 +0000

    [tests,VulkanCube] Some more Vulkan test code, enumerating physical devices, querying their features, and creating a logical device (which doesn't work yet)

--- tests/VulkanCube/source/main.cpp
+++ tests/VulkanCube/source/main.cpp
@@ -15,11 +15,58 @@ WinMain(
     memset(&createInfo, 0, sizeof(createInfo));
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO; // required
     //memset(&allocCbs, 0, sizeof(allocCbs));
-    auto result = createInstanceFn(&createInfo, nullptr, &instance);
-    if (VK_SUCCESS == result)
+    auto createInstanceRes = createInstanceFn(&createInfo, nullptr, &instance);
+    if (VK_SUCCESS != createInstanceRes)
     {
-        auto destroyInstanceFn = reinterpret_cast<PFN_vkDestroyInstance>(vkGetInstanceProcAddr(instance, "vkDestroyInstance"));
-        destroyInstanceFn(instance, nullptr);
+        return -1;
     }
+
+    // enumerate physical devices
+    auto enumPhysDevicesFn = reinterpret_cast<PFN_vkEnumeratePhysicalDevices>(vkGetInstanceProcAddr(instance, "vkEnumeratePhysicalDevices"));
+    uint32_t numPhysicalDevices = 0;
+
+    // get number of physical devices
+    auto enumPhysDevicesRes = enumPhysDevicesFn(instance, &numPhysicalDevices, nullptr);
+    if (VK_SUCCESS != enumPhysDevicesRes)
+    {
+        // TODO: need some RAII on the instance
+        return -1;
+    }
+    auto physicalDevices = new VkPhysicalDevice[numPhysicalDevices];
+    enumPhysDevicesRes = enumPhysDevicesFn(instance, &numPhysicalDevices, physicalDevices);
+    if (VK_SUCCESS != enumPhysDevicesRes)
+    {
+        // TODO: need some RAII on the instance
+        return -1;
+    }
+
+    auto getPhysDeviceFeaturesFn = reinterpret_cast<PFN_vkGetPhysicalDeviceFeatures>(vkGetInstanceProcAddr(instance, "vkGetPhysicalDeviceFeatures"));
+    for (auto i = 0; i < numPhysicalDevices; ++i)
+    {
+        auto device = physicalDevices[i];
+        VkPhysicalDeviceFeatures features;
+        getPhysDeviceFeaturesFn(device, &features);
+    }
+
+    // create a logical device
+    auto createDeviceFn = reinterpret_cast<PFN_vkCreateDevice>(vkGetInstanceProcAddr(instance, "vkCreateDevice"));
+    VkDeviceCreateInfo deviceCreateInfo;
+    memset(&deviceCreateInfo, 0, sizeof(deviceCreateInfo));
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    VkDevice device;
+    auto createDeviceRes = createDeviceFn(physicalDevices[0], &deviceCreateInfo, nullptr, &device);
+    if (VK_SUCCESS != createDeviceRes)
+    {
+        return -1;
+    }
+
+    // destroy the logical device
+    auto destroyDeviceFn = reinterpret_cast<PFN_vkDestroyDevice>(vkGetInstanceProcAddr(instance, "vkDestroyDevice"));
+    destroyDeviceFn(device, nullptr);
+
+    delete[] physicalDevices;
+
+    auto destroyInstanceFn = reinterpret_cast<PFN_vkDestroyInstance>(vkGetInstanceProcAddr(instance, "vkDestroyInstance"));
+    destroyInstanceFn(instance, nullptr);
     return 0;
 }
