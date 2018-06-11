f46617c2f4792990bd182b4ab9c25a9296dbfc72
Mark Final <markfinal@hotmail.com>
2018-06-11 22:30:34 +0100

    Adding SPIRVHeaders

---
+++ packages/SPIRVHeaders/bam/Scripts/SPIRVHeaders.cs
@@ -0,0 +1,25 @@
+using Bam.Core;
+namespace SPIRVHeaders
+{
+    class SPIRVHeaders : C.HeaderLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            var headers = this.CreateHeaderContainer("$(packagedir)/include/**.hpp");
+            headers.AddFiles("$(packagedir)/include/**.h");
+
+            this.PublicPatch((Settings, appliedTo) =>
+            {
+                var compiler = Settings as C.ICommonCompilerSettings;
+                if (null != compiler)
+                {
+                    compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/include"));
+                }
+            });
+        }
+    }
+}
