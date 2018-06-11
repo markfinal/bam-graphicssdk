e78e572a389f2817019a261222ec8d0375c65009
Mark Final <markfinal@hotmail.com>
2018-06-11 20:25:45 +0100

    Added SPIRVCross package

---
+++ packages/SPIRVCross/bam/Scripts/SPIRVCross.cs
@@ -0,0 +1,25 @@
+using Bam.Core;
+namespace SPIRVCross
+{
+    class SPIRVCross : C.HeaderLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            var header = this.CreateHeaderContainer("$(packagedir)/include/**.h");
+            header.AddFiles("$(packagedir)/include/**.hpp");
+
+            this.PublicPatch((Settings, appliedTo) =>
+            {
+                var compiler = Settings as C.ICommonCompilerSettings;
+                if (null != compiler)
+                {
+                    compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/include"));
+                }
+            });
+        }
+    }
+}
