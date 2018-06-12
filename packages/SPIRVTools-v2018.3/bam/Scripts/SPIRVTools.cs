1df06b03a57340a1a5c5837140609f5291c71de7
Mark Final <markfinal@hotmail.com>
2018-06-12 08:10:46 +0100

    Generate DebugInfo.h header

--- packages/SPIRVTools-v2018.3/bam/Scripts/SPIRVTools.cs
+++ packages/SPIRVTools-v2018.3/bam/Scripts/SPIRVTools.cs
@@ -13,12 +13,18 @@ namespace SPIRVTools
             this.DependsOn(spirvheaders);
 
             this.Macros.Add("Version", "unified1");
-            this.Macros.Add("GenerateGrammarTables", "$(packagedir)/utils/generate_grammar_tables.py");
+            this.Macros.Add("GenerateGrammarTablesPy", "$(packagedir)/utils/generate_grammar_tables.py");
             this.Macros.Add("GrammarJsonFile", this.CreateTokenizedString("$(0)/spirv/$(Version)/spirv.core.grammar.json", new []{spirvheaders.Macros["IncludeDir"]}));
             this.Macros.Add("DebugGrammarFile", "$(packagedir)/source/extinst.debuginfo.grammar.json");
             this.Macros.Add("GrammarEnumStringMappingIncFile", "$(packagebuilddir)/$(moduleoutputdir)/enum_string_mapping.inc");
 
-            this.Macros.Add("Arguments", this.CreateTokenizedString("$(GenerateGrammarTables) --spirv-core-grammar=$(GrammarJsonFile) --extinst-debuginfo-grammar=$(DebugGrammarFile) --extension-enum-output=$(0) --enum-string-mapping-output=$(GrammarEnumStringMappingIncFile)", new [] {this.OutputPath}));
+            var arguments = new System.Text.StringBuilder();
+            arguments.Append("$(GenerateGrammarTablesPy) ");
+            arguments.Append("--spirv-core-grammar=$(GrammarJsonFile) ");
+            arguments.Append("--extinst-debuginfo-grammar=$(DebugGrammarFile) ");
+            arguments.Append("--extension-enum-output=$(0) ");
+            arguments.Append("--enum-string-mapping-output=$(GrammarEnumStringMappingIncFile) ");
+            this.Macros.Add("Arguments", this.CreateTokenizedString(arguments.ToString(), new [] {this.OutputPath}));
         }
 
         protected override Bam.Core.TokenizedString OutputPath
@@ -26,6 +32,54 @@ namespace SPIRVTools
             get
             {
                 return this.CreateTokenizedString("$(packagebuilddir)/$(moduleoutputdir)/extension_enum.inc");
+            }
+        }
+
+        protected override string Contents
+        {
+            get
+            {
+                var output = Bam.Core.OSUtilities.RunExecutable(
+                    "python",
+                    this.Macros["Arguments"].ToString()
+                );
+                Bam.Core.Log.MessageAll("Running 'python {0}'", this.Macros["Arguments"].ToString());
+                if (!System.String.IsNullOrEmpty(output))
+                {
+                    Bam.Core.Log.MessageAll("\t{0}", output);
+                }
+                return null;
+            }
+        }
+    }
+
+    class DebugInfo : C.ProceduralHeaderFile
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            var spirvheaders = Bam.Core.Graph.Instance.FindReferencedModule<SPIRVHeaders.SPIRVHeaders>();
+            this.DependsOn(spirvheaders);
+
+            this.Macros.Add("GenerateLanguageHeadersPy", "$(packagedir)/utils/generate_language_headers.py");
+            this.Macros.Add("DebugGrammarFile", "$(packagedir)/source/extinst.debuginfo.grammar.json");
+
+            var arguments = new System.Text.StringBuilder();
+            arguments.Append("$(GenerateLanguageHeadersPy) ");
+            arguments.Append("--extinst-name=DebugInfo ");
+            arguments.Append("--extinst-grammar=$(DebugGrammarFile) ");
+            arguments.Append("--extinst-output-base=$(packagebuilddir)/$(moduleoutputdir)/DebugInfo ");
+            this.Macros.Add("Arguments", this.CreateTokenizedString(arguments.ToString()));
+        }
+
+        protected override Bam.Core.TokenizedString OutputPath
+        {
+            get
+            {
+                return this.CreateTokenizedString("$(packagebuilddir)/$(moduleoutputdir)/DebugInfo.h");
             }
         }
 
@@ -64,6 +118,10 @@ namespace SPIRVTools
             var extensionEnumInc = Bam.Core.Graph.Instance.FindReferencedModule<ExtensionEnumInc>();
             source.DependsOn(extensionEnumInc);
             source.UsePublicPatches(extensionEnumInc);
+
+            var debugInfoHeader = Bam.Core.Graph.Instance.FindReferencedModule<DebugInfo>();
+            source.DependsOn(debugInfoHeader);
+            source.UsePublicPatches(debugInfoHeader);
 
             source.PrivatePatch(settings =>
             {
