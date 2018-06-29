313bd3cee001ac20feb5b4928c2b0fc05a4ea55d
Mark Final <markfinal@hotmail.com>
2018-06-29 22:34:55 +0100

    Renaming source file after class rename

--- tests/OpenGLTriangle/source/application.cpp
+++ tests/OpenGLTriangle/source/application.cpp
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "renderer.h"
 #include "errorhandler.h"
 #include "common.h"
-#include "window.h"
+#include "appwindow.h"
 
 #include <string>
 
