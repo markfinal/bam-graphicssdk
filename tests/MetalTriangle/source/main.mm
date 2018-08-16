a6d0d017e3adebc6129ef27e9584753301ef114c
Mark Final <markfinal@hotmail.com>
2018-08-16 08:44:16 +0100

    Logging on the view controller for understanding

--- tests/MetalTriangle/source/main.mm
+++ tests/MetalTriangle/source/main.mm
@@ -9,19 +9,46 @@
 
 @interface MetalViewController : NSViewController
 {}
+-(void)viewDidLoad;
+-(void)viewWillAppear;
 -(void)viewDidAppear;
+-(void)updateViewConstraints;
+-(void)viewWillLayout;
+-(void)viewDidLayout;
+-(void)viewWillDisappear;
+-(void)viewDidDisappear;
 @end
 
 @implementation MetalViewController : NSViewController
+-(void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
+-(void)viewWillAppear
+{
+    [super viewWillAppear];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
 -(void)viewDidAppear
 {
-    // https://www.haroldserrano.com/blog/getting-started-with-metal-api
+    [super viewDidAppear];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+    /*
+    // https://www.haroldserrano.com/blog/getting-started-with-metal-api (iOS)
+    // https://developer.apple.com/documentation/metal/hello_triangle
     auto mtlDevice = MTLCreateSystemDefaultDevice();
     auto metalLayer = [CAMetalLayer layer];
     metalLayer.device = mtlDevice;
     metalLayer.pixelFormat = MTLPixelFormatBGRA8Unorm;
     metalLayer.frame = self.view.bounds;
     [self.view.layer addSublayer:metalLayer];
+
+    auto mtlCommandQueue = [mtlDevice newCommandQueue];
+    auto mtlCommandBuffer = [mtlCommandQueue commandBuffer];
+
+    auto renderPassDescriptor = [[MTLRenderPassDescriptor alloc] init];
+    auto mtlRenderEncoder = [mtlCommandBuffer renderCommandEncoderWithDescriptor:renderPassDescriptor];
 
     auto mtlLibrary = [mtlDevice newDefaultLibrary];
     auto vertexProgram = [mtlLibrary newFunctionWithName:@"clip_space_colour_vertex_function"];
@@ -30,11 +57,10 @@
     auto mtlRenderPipelineDescriptor = [[MTLRenderPipelineDescriptor alloc] init];
     [mtlRenderPipelineDescriptor setVertexFunction:vertexProgram];
     [mtlRenderPipelineDescriptor setFragmentFunction:fragmentProgram];
-
     mtlRenderPipelineDescriptor.colorAttachments[0].pixelFormat=MTLPixelFormatBGRA8Unorm;
 
     auto renderPipelineState = [mtlDevice newRenderPipelineStateWithDescriptor:mtlRenderPipelineDescriptor error:nil];
-    (void)renderPipelineState;
+    [mtlRenderEncoder setRenderPipelineState:renderPipelineState];
 
     static float quadVertexData[] =
     {
@@ -47,15 +73,46 @@
         -0.5,  0.5, 0.0, 1.0
     };
 
-    auto vertexBuffer = [mtlDevice newBufferWithBytes:quadVertexData length:sizeof(quadVertexData) options:MTLResourceOptionCPUCacheModeDefault];
-    (void)vertexBuffer;
+    [mtlRenderEncoder setVertexBytes:quadVertexData
+                    length:sizeof(quadVertexData)
+                    atIndex:0];
+
+    [mtlRenderEncoder drawPrimitives:MTLPrimitiveTypeTriangle
+                      vertexStart:0
+                      vertexCount:3];
+
+    //auto vertexBuffer = [mtlDevice newBufferWithBytes:quadVertexData length:sizeof(quadVertexData) options:MTLResourceOptionCPUCacheModeDefault];
+    //(void)vertexBuffer;
 
     //auto displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(renderScene)];
 
     //[displayLink addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-
-    auto mtlCommandQueue = [mtlDevice newCommandQueue];
-    (void)mtlCommandQueue;
+    */
+}
+-(void)updateViewConstraints
+{
+    [super updateViewConstraints];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
+-(void)viewWillLayout
+{
+    [super viewWillLayout];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
+-(void)viewDidLayout
+{
+    [super viewDidLayout];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
+-(void)viewWillDisappear
+{
+    [super viewWillDisappear];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
+}
+-(void)viewDidDisappear
+{
+    [super viewDidDisappear];
+    NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
 }
 @end
 
