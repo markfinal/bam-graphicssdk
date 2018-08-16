e11249b5970416718307a8955f3e7b6c2e76933f
Mark Final <markfinal@hotmail.com>
2018-08-16 09:40:30 +0100

    Combined MTKViewDelegate implementation into the single view controller

--- tests/MetalTriangle/source/main.mm
+++ tests/MetalTriangle/source/main.mm
@@ -9,7 +9,7 @@
 
 /* ---------------------------------------------------------------------- */
 
-@interface MetalViewController : NSViewController
+@interface MetalViewController : NSViewController<MTKViewDelegate>
 {}
 -(void)viewDidLoad;
 -(void)viewWillAppear;
@@ -19,6 +19,10 @@
 -(void)viewDidLayout;
 -(void)viewWillDisappear;
 -(void)viewDidDisappear;
+/* ---- */
+- (void)drawInMTKView:(nonnull MTKView *)view;
+- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size;
+- (void)encodeWithCoder:(nonnull NSCoder *)aCoder;
 @end
 
 /* ---------------------------------------------------------------------- */
@@ -118,19 +122,9 @@
     [super viewDidDisappear];
     NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
 }
-@end
 
-/* ---------------------------------------------------------------------- */
+/* ---- */
 
-@interface MetalViewDelegate : NSViewController<MTKViewDelegate>
-- (void)drawInMTKView:(nonnull MTKView *)view;
-- (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size;
-- (void)encodeWithCoder:(nonnull NSCoder *)aCoder;
-@end
-
-/* ---------------------------------------------------------------------- */
-
-@implementation MetalViewDelegate : NSViewController
 - (void)drawInMTKView:(nonnull MTKView *)view
 {
     NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
@@ -147,6 +141,7 @@
     NSLog((@"%s [Line %d] "), __PRETTY_FUNCTION__, __LINE__);
     (void)aCoder;
 }
+
 @end
 
 /* ---------------------------------------------------------------------- */
@@ -160,13 +155,13 @@ main()
     std::unique_ptr<WindowLibrary::GraphicsWindow> metalWindow(new WindowLibrary::GraphicsWindow);
     metalWindow->init(512, 512, "Metal Example");
 
-    auto frame = NSMakeRect(0, 0, metalWindow->width(), metalWindow->height());
-    auto metal_view = [[MTKView alloc] initWithFrame:frame];
-    metal_view.delegate = [[MetalViewDelegate alloc] init];
-    [[metalWindow->getNativeWindowHandle() contentView] addSubview:metal_view];
-
+    auto metal_view = [[MTKView alloc] initWithFrame:NSMakeRect(0, 0, metalWindow->width(), metalWindow->height())];
     auto view_controller = [[MetalViewController alloc] init];
+
     view_controller.view = metal_view;
+    metal_view.delegate = view_controller;
+
+    [[metalWindow->getNativeWindowHandle() contentView] addSubview:metal_view];
 
     metalWindow->show();
 
