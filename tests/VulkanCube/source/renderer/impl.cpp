38f44f92d9f9625a6aa4e125695ba11d9ce9eeaf
Mark Final <markfinal@hotmail.com>
2018-11-28 21:12:15 +0000

    [tests,VulkanCube] Correctly initialise the debug report flags

--- tests/VulkanCube/source/renderer/impl.cpp
+++ tests/VulkanCube/source/renderer/impl.cpp
@@ -251,10 +251,10 @@ Renderer::Impl::init_debug_callback()
     memset(&createInfo, 0, sizeof(createInfo));
     createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CREATE_INFO_EXT;
     createInfo.flags =
-        VK_DEBUG_REPORT_INFORMATION_BIT_EXT ||
-        VK_DEBUG_REPORT_WARNING_BIT_EXT ||
-        VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT ||
-        VK_DEBUG_REPORT_ERROR_BIT_EXT ||
+        VK_DEBUG_REPORT_INFORMATION_BIT_EXT |
+        VK_DEBUG_REPORT_WARNING_BIT_EXT |
+        VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT |
+        VK_DEBUG_REPORT_ERROR_BIT_EXT |
         VK_DEBUG_REPORT_DEBUG_BIT_EXT;
     createInfo.pfnCallback = debug_callback;
     ::VkDebugReportCallbackEXT callback;
