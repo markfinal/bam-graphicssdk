89ea00ee4fb9d9496ae02494e9c6c07afa810192
Mark Final <markfinal@hotmail.com>
2019-05-29 18:51:01 +0100

    [CI] Enabling continuous integration

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -1,8 +1,16 @@
-from testconfigurations import TestSetup, visualc64, mingw32, gcc64, clang64
+from testconfigurations import TestSetup, visualc, visualc64, visualc32, mingw32, gcc, gcc64, gcc32, clang, clang32, clang64
 
 def configure_repository():
     configs = {}
-    configs["Direct3DTriangle"] = TestSetup(win={"Native":[visualc64],"VSSolution":[visualc64],"MakeFile":[visualc64]})
-    configs["OpenGLTriangle"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
-    configs["RenderTextureAndProcessor"] = TestSetup(win={"Native":[visualc64,mingw32],"VSSolution":[visualc64],"MakeFile":[visualc64,mingw32]})
+    configs["Direct3DTriangle"] = TestSetup(win={"Native": [visualc64, visualc32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32]})
+    configs["OpenGLTriangle"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                          linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                          osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["RenderTextureAndProcessor"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]})
+    configs["VulkanTriangle"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                          osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["MetalTriangle"] = TestSetup(osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    configs["WindowLibrary"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
