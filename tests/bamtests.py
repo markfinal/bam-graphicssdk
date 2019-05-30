5ac5eddf19df54444a940d167695e849eccfab26
Mark Final <markfinal@hotmail.com>
2019-05-30 07:07:42 +0100

    [tests] Comment out the WindowLibrary test for now. It is built part of other tests

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -10,7 +10,7 @@ def configure_repository():
     configs["VulkanTriangle"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
                                           osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["MetalTriangle"] = TestSetup(osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
-    configs["WindowLibrary"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
-                                         linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
-                                         osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
+    #configs["WindowLibrary"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+    #                                     linux={"Native": [gcc64, gcc32], "MakeFile": [gcc64, gcc32]},
+    #                                     osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     return configs
