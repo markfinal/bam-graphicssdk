f95247692fd5e09f55a5c8b1c3d08ad0b5a1f23d
Mark Final <markfinal@hotmail.com>
2019-06-07 12:05:34 +0100

    [tests] There are some oddities in declaring types in 32-bit builds of Vulkan, so don't test this

--- tests/bamtests.py
+++ tests/bamtests.py
@@ -7,7 +7,7 @@ def configure_repository():
                                           linux={"Native": [gcc64], "MakeFile": [gcc64]}, # 64-bit only because https://askubuntu.com/questions/695604/libglu1-mesa-dev-how-to-keep-both-i386-and-amd64-packages
                                           osx={"Native": [clang64, clang32], "MakeFile": [clang64, clang32], "Xcode": [clang64, clang32]})
     configs["RenderTextureAndProcessor"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]})
-    configs["VulkanTriangle"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
+    configs["VulkanTriangle"] = TestSetup(win={"Native": [visualc64], "VSSolution": [visualc64], "MakeFile": [visualc64]},
                                           osx={"Native": [clang64], "MakeFile": [clang64], "Xcode": [clang64]})
     configs["MetalTriangle"] = TestSetup(osx={"Native": [clang64], "MakeFile": [clang64], "Xcode": [clang64]})
     #configs["WindowLibrary"] = TestSetup(win={"Native": [visualc64, visualc32, mingw32], "VSSolution": [visualc64, visualc32], "MakeFile": [visualc64, visualc32, mingw32]},
